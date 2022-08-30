979226a148d5b6dddee06641fa8b135d06e4cfa6
Michel Jung <michel.jung@zkb.ch>
2022-08-30 09:30:47 +0200

    Reorder tests

--- src/test/kotlin/WeekendsUnitTest.kt
+++ src/test/kotlin/WeekendsUnitTest.kt
@@ -4,30 +4,6 @@ import java.time.LocalDate
 import java.time.YearMonth
 
 internal class WeekendsUnitTest {
-  @Test
-  fun `January 2022 has 5 weekends`() {
-    assertEquals(5, WeekendUtils.countNumberOfWeekends(YearMonth.of(2022, 1)))
-  }
-
-  @Test
-  fun `February 2022 has 4 weekends`() {
-    assertEquals(4, WeekendUtils.countNumberOfWeekends(YearMonth.of(2022, 2)))
-  }
-
-  @Test
-  fun `October 2021 has 5 weekends`() {
-    assertEquals(5, WeekendUtils.countNumberOfWeekends(YearMonth.of(2021, 10)))
-  }
-
-  @Test
-  fun `July 2nd is first saturday of July 2022`() {
-    assertEquals(LocalDate.of(2022, 7, 2), WeekendUtils.firstSaturday(YearMonth.of(2022, 7)))
-  }
-
-  @Test
-  fun `July 3rd is first saturday of July 2021`() {
-    assertEquals(LocalDate.of(2021, 7, 3), WeekendUtils.firstSaturday(YearMonth.of(2021, 7)))
-  }
 
   @Test
   fun `2022-01-22 is weekend`() {
@@ -42,5 +18,35 @@ internal class WeekendsUnitTest {
   @Test
   fun `July 25th is last sunday of July 2021`() {
     assertEquals(LocalDate.of(2021, 7, 25), WeekendUtils.firstSaturday(YearMonth.of(2021, 7)))
+  }
+
+  @Test
+  fun `July 2nd is first saturday of July 2022`() {
+    assertEquals(LocalDate.of(2022, 7, 2), WeekendUtils.firstSaturday(YearMonth.of(2022, 7)))
+  }
+
+  @Test
+  fun `July 3rd is first saturday of July 2021`() {
+    assertEquals(LocalDate.of(2021, 7, 3), WeekendUtils.firstSaturday(YearMonth.of(2021, 7)))
+  }
+
+  @Test
+  fun `January 2022 has 5 weekends`() {
+    assertEquals(5, WeekendUtils.countNumberOfWeekends(YearMonth.of(2022, 1)))
+  }
+
+  @Test
+  fun `February 2022 has 4 weekends`() {
+    assertEquals(4, WeekendUtils.countNumberOfWeekends(YearMonth.of(2022, 2)))
+  }
+
+  @Test
+  fun `April 2021 has 4 weekends`() {
+    assertEquals(4, WeekendUtils.countNumberOfWeekends(YearMonth.of(2021, 4)))
+  }
+
+  @Test
+  fun `October 2021 has 5 weekends`() {
+    assertEquals(5, WeekendUtils.countNumberOfWeekends(YearMonth.of(2021, 10)))
   }
 }
